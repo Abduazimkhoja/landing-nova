db2b5cc8b51f6815ac22e05a84a01ce6e709dd6b
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-10-29 06:27:46 +0500

    refactor: country card contain link

--- src/components/ui/country-card/index.tsx
+++ src/components/ui/country-card/index.tsx
@@ -1,10 +1,10 @@
+import { DOMAIN } from "@/helpers/api.helper";
 import { LangTuple } from "@/helpers/languages.helper";
 import Image from "next/image";
 import { FC, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { countryCardsData } from "./country-data";
 import styles from "./style.module.scss";
-import { DOMAIN } from "@/helpers/api.helper";
 
 type Props = {
   className?: string;
@@ -24,28 +24,30 @@ const CountryCard: FC<Props> = ({ className, ...rest }) => {
       <ul className={styles.list}>
         {countryCardsData?.cards?.map(({ country, link, ...labels }) => (
           <li className={styles.item} key={labels[`label_${language}`]}>
-            <div className={styles.card}>
-              <Image
-                width="800"
-                height="400"
-                src={`${DOMAIN}/uploads/assets/images/${country}-card.webp`}
-                alt="country"
-                className={styles.img}
-              />
-              <a target="_blank" href={link} className={styles.link}>
+            <a target="_blank" href={link}>
+              <div className={styles.card}>
                 <Image
-                  height="28"
-                  width="28"
-                  src={`/icons/flags/${country}-flag.svg`}
-                  alt="flag"
-                  className={styles["icon-flag"]}
+                  width="800"
+                  height="400"
+                  src={`${DOMAIN}/uploads/assets/images/${country}-card.webp`}
+                  alt="country"
+                  className={styles.img}
                 />
-                <h3 className={styles.name}>
-                  {labels[`label_${language}`]}
-                  <Image width="10" height="14" className={styles["icon-arrow"]} src="/icons/arrow.svg" alt="arrow" />
-                </h3>
-              </a>
-            </div>
+                <div className={styles.link}>
+                  <Image
+                    height="28"
+                    width="28"
+                    src={`/icons/flags/${country}-flag.svg`}
+                    alt="flag"
+                    className={styles["icon-flag"]}
+                  />
+                  <h3 className={styles.name}>
+                    {labels[`label_${language}`]}
+                    <Image width="10" height="14" className={styles["icon-arrow"]} src="/icons/arrow.svg" alt="arrow" />
+                  </h3>
+                </div>
+              </div>
+            </a>
           </li>
         ))}
       </ul>
