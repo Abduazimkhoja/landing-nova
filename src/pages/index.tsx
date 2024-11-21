52e6a7e16ca34d6126558ea95e37a375c1df04d3
shukhratovich-a <shukhratovich138@gmail.com>
2024-11-21 23:46:29 +0500

    refactor: correct home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,19 @@ import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-// import { IBanner } from "@/types/banner.interface";
-// import { IAbout } from "@/types/about.interface";
-// import { ContactTypeEnum } from "@/enums/contact-type.enum";
-// import { IContact } from "@/types/contact.interface";
+import { IBanner } from "@/types/banner.interface";
+import { IAbout } from "@/types/about.interface";
+import { ContactTypeEnum } from "@/enums/contact-type.enum";
+import { IContact } from "@/types/contact.interface";
 
-// import { getAll as getAllBanners } from "@/api/banner.api";
-// import { getAll as getAllAbouts } from "@/api/about.api";
+import { getAll as getAllBanners } from "@/api/banner.api";
+import { getAll as getAllAbouts } from "@/api/about.api";
 
 import { withLayout } from "@/layout/layout";
 
-// import { HomeView } from "@/views";
+import { HomeView } from "@/views";
 
-// import { getByType as getContactByType } from "@/api/contact.api";
+import { getByType as getContactByType } from "@/api/contact.api";
 
 const HomePage: FC<HomePageProps> = ({ _nextI18Next, ...rest }) => {
   return (
@@ -24,35 +24,32 @@ const HomePage: FC<HomePageProps> = ({ _nextI18Next, ...rest }) => {
         <title>NOVA Plastik</title>
       </Head>
 
-      <div>{`It works don't worry)`}</div>
-      {/* <HomeView {...rest} /> */}
+      <HomeView {...rest} />
     </>
   );
 };
 
 export const getServerSideProps: GetServerSideProps<HomePageProps> = async ({ locale }) => {
   try {
-    // const {
-    //   data: { data: banners },
-    // } = await getAllBanners({ language: locale });
+    const {
+      data: { data: banners },
+    } = await getAllBanners({ language: locale });
 
-    // const { data: factories } = await getContactByType(ContactTypeEnum.FACTORY, locale);
+    const { data: factories } = await getContactByType(ContactTypeEnum.FACTORY, locale);
 
-    // const {
-    //   data: { data: abouts },
-    // } = await getAllAbouts({ language: locale });
+    const {
+      data: { data: abouts },
+    } = await getAllAbouts({ language: locale });
 
     return {
       props: {
-        // banners,
-        // abouts,
-        // factories,
+        banners,
+        abouts,
+        factories,
         ...(await serverSideTranslations(String(locale))),
       },
     };
-  } catch (e) {
-    console.log(e);
-
+  } catch {
     return {
       notFound: true,
     };
@@ -62,7 +59,7 @@ export const getServerSideProps: GetServerSideProps<HomePageProps> = async ({ lo
 export default withLayout(HomePage);
 
 export interface HomePageProps extends Record<string, unknown> {
-  // banners: IBanner[];
-  // abouts: IAbout[];
-  // factories: IContact[];
+  banners: IBanner[];
+  abouts: IAbout[];
+  factories: IContact[];
 }
