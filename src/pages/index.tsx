1024fc09b1e1ef3fb75490ae2986d32a9ec6e2ff
shukhratovich-a <shukhratovich138@gmail.com>
2024-11-21 22:12:56 +0500

    refactor: change rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,19 @@
-import { GetStaticProps } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Head from "next/head";
 import { FC } from "react";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import { IBanner } from "@/types/banner.interface";
-// import { ICategory } from "@/types/category.interface";
+import { IAbout } from "@/types/about.interface";
+import { ContactTypeEnum } from "@/enums/contact-type.enum";
+import { IContact } from "@/types/contact.interface";
 
 import { getAll as getAllBanners } from "@/api/banner.api";
-// import { getAll as getAllCategories } from "@/api/category.api";
-// import { getAll as getAllCatalogs } from "@/api/catalog.api";
-// import { getAll as getAllCertificates } from "@/api/certificate.api";
-// import { getAll as getAllPosts } from "@/api/post.api";
 import { getAll as getAllAbouts } from "@/api/about.api";
 
 import { withLayout } from "@/layout/layout";
 
-// import { ICatalog } from "@/types/catalog.interface";
-// import { ICertificate } from "@/types/certificate.interface";
-// import { IPost } from "@/types/post.interface";
 import { HomeView } from "@/views";
-import { IAbout } from "@/types/about.interface";
-import { ContactTypeEnum } from "@/enums/contact-type.enum";
-import { IContact } from "@/types/contact.interface";
 
 import { getByType as getContactByType } from "@/api/contact.api";
 
@@ -37,25 +29,12 @@ const HomePage: FC<HomePageProps> = ({ _nextI18Next, ...rest }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps<HomePageProps> = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps<HomePageProps> = async ({ locale }) => {
   try {
     const {
       data: { data: banners },
     } = await getAllBanners({ language: locale });
-    
-    // const {
-    //   data: { data: categories },
-    // } = await getAllCategories({ language: locale, limit: 12 });
-    // const {
-    //   data: { data: catalogs },
-    // } = await getAllCatalogs({ language: locale, limit: 12 });
-    // const {
-    //   data: { data: posts },
-    // } = await getAllPosts({ language: locale, limit: 12 });
 
-    // const { data: certificates } = await getAllCertificates({ language: locale });
-
-    // const { data: centrals } = await getContactByType(ContactTypeEnum.CENTRAL, locale);
     const { data: factories } = await getContactByType(ContactTypeEnum.FACTORY, locale);
 
     const {
@@ -65,18 +44,14 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async ({ locale }) 
     return {
       props: {
         banners,
-        // categories,
-        // certificates,
-        // catalogs,
-        // posts,
         abouts,
-        // centrals,
         factories,
         ...(await serverSideTranslations(String(locale))),
       },
-      revalidate: 1,
     };
-  } catch {
+  } catch (e) {
+    console.log(e);
+
     return {
       notFound: true,
     };
@@ -87,11 +62,6 @@ export default withLayout(HomePage);
 
 export interface HomePageProps extends Record<string, unknown> {
   banners: IBanner[];
-  // certificates: ICertificate[];
-  // categories: ICategory[];
-  // posts: IPost[];
-  // catalogs: ICatalog[];
   abouts: IAbout[];
-  // centrals: IContact[];
   factories: IContact[];
 }
